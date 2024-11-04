aa01d5b40349d07ee3feef396cdf9268a82fe69b
Abhimanyu <javaabhimanyu@gmail.com>
2024-11-04 11:58:14 +0530

    please check
    Done

---
+++ js/main.js
@@ -0,0 +1,132 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonial carousel
+    $(".testimonial-carousel-1").owlCarousel({
+        loop: true,
+        dots: false,
+        margin: 25,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 0,
+        autoplaySpeed: 10000,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            575: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            991: {
+                items: 3
+            }
+        }
+    });
+
+    $(".testimonial-carousel-2").owlCarousel({
+        loop: true,
+        dots: false,
+        rtl: true,
+        margin: 25,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 0,
+        autoplaySpeed: 10000,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            575: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            991: {
+                items: 3
+            }
+        }
+    });
+
+})(jQuery);
+
+// FAQ Here Start
+let question = document.querySelectorAll(".question");
+
+question.forEach(question => {
+    question.addEventListener("click", event => {
+        const active = document.querySelector(".question.active");
+        if (active && active !== question) {
+            active.classList.toggle("active");
+            active.nextElementSibling.style.maxHeight = 0;
+        }
+        question.classList.toggle("active");
+        const answer = question.nextElementSibling;
+        if (question.classList.contains("active")) {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        } else {
+            answer.style.maxHeight = 0;
+        }
+    })
+})
+
+// FAQ Here End
